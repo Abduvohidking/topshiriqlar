a1fec0a7dab5a251b4cec175fb96b6fced5fe257
Abduvohid_Ahmadjonov <84452468+Abduvohidking@users.noreply.github.com>
2021-06-22 23:26:43 +0300

    Update toplar.py
    9 tasini tugatdim  faqat

--- toplar.py
+++ toplar.py
@@ -1,6 +1,143 @@
-# cars = ["audi", "tesla","hundai"]
-# cars.append("BMW")
-# cars.insert(2,"Dewoo")
-# cars.reverse()
-# print(cars)
-print(2/0)
+# topshiriq1
+
+# a = int(input("a="))
+# if a>0 :
+#     print("a soni musbat son")
+# elif a==0:
+#     print("Asoni no;ga teng")
+# else:
+#     print("a soni manfiy ")
+
+# topshiriq 2 va 3
+
+# a = int(input("a="))
+# if a%2==0:
+#     print("Son juft son")
+# else:
+#     print("Son toq")
+
+# 4 topshiriq
+
+# a = int(input("a="))
+# b = int(input("b="))
+# c = int(input("c="))
+
+# if a>b>c:
+#     print("a>b>c")
+# elif b>a>c:
+#     print("b>a>c")
+# elif c>a>b:
+#     print("c>a>b")
+# elif a>c>b:
+#     print("a>c>b")
+# elif b>c>a:
+#     print("b>c>a")
+# elif c>b>a:
+#     print("c>b>a")
+
+
+# 5 topshiriq
+
+# a = int(input("a="))
+# b = int(input("b="))
+
+# if a%2==0 and b%2==0 :
+#     print("sonlar juft")
+# elif a%2==0:
+#     print(f"a soni juft son")
+# elif b%2==0:
+#     print(f"b soni juft")
+# else:
+#     print("sonlar top sonlar")
+
+# topshiriq 6
+
+
+# a = int(input("a="))
+# b = int(input("b="))
+# c = int(input("c="))
+
+# if b>c>a or b>a>c :
+#     print("Eng katta son b")
+# elif c>b>a or c>a>b :
+#     print("Eng katta son c")
+# else :
+#     print("Eng kattaa son a")
+
+# topshiriq 7
+
+# a = int(input("a="))
+# b = int(input("b="))
+# c = int(input("c="))
+
+# if a!=b and a!=c and b!=c:
+#     print("0")
+# elif a==b==c:
+#     print("3")
+# elif a==c or b==c or a==b :
+#     print("2") 
+
+# topshiriq 8
+
+# a = int(input("a="))
+# b = int(input("b="))
+# c = int(input("c="))
+# d = int(input("d="))
+# f = int(input("f=")) 
+
+# if b>c>a>d>f or b>c>a>f>d or b>c>d>a>f or b>c>d>f>a or b>c>f>a>d or b>a>f>d>c or b>a>c>d>f or b>a>c>f>d or b>a>d>c>f or b>a>d>f>c or  b>a>f>d>c or b>a>c>d>f or b>a>c>f>d or b>a>d>c>f or b>a>d>f>c  :
+#     print("Eng katta son b")
+# elif c>b>a or c>a>b :
+#     print("Eng katta son c")
+# else :
+#     print("Eng kattaa son a")
+
+
+# # topshiriq 9
+
+# a = int(input("oy tartib raqami="))
+
+# if a==1:
+#     print("bu oyda 31 kun bor")
+# elif a==2:
+#     print("bu oyda 28 kun bor")
+# elif a==3:
+#     print("bu oyda 31 kun bor")
+# elif a==4:
+#     print("bu oyda 30 kun bor")
+# elif a==5:
+#     print("bu oyda 31 kun bor")
+# elif a==6:
+#     print("bu oyda 30 kun bor")
+# elif a==7:
+#     print("bu oyda 31 kun bor")
+# elif a==8:
+#     print("bu oyda 31 kun bor")
+# elif a==9:
+#     print("bu oyda 30 kun bor")
+# elif a==10:
+#     print("bu oyda 31 kun bor")
+# elif a==11:
+#     print("bu oyda 30 kun bor")
+# elif a==12:
+#     print("bu oyda 31 kun bor")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
